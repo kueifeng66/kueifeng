913449f934338f0efa102bc84adb04547cc75297
kueifeng66 <116413606+kueifeng66@users.noreply.github.com>
2024-03-29 15:23:06 +0800

    Update script.js

--- script.js
+++ script.js
@@ -225,7 +225,7 @@ function createCalendar(year, month) {
 
   const headerCell = document.createElement('div');
   headerCell.classList.add('header-cell');
-  headerCell.textContent = `${year_now} 年 ${month} 月  `;
+  headerCell.textContent = `${year_now}年  ${month}月  `;
   header.appendChild(headerCell); 
 
   const date = `${year}-${month}-${day}`;
@@ -375,7 +375,7 @@ function change() {
       }
     });
     const headerCell = document.querySelector('.header-cell');
-    headerCell.textContent = `${year} 年 ${month} 月  `;
+    headerCell.textContent = `${year}年  ${month}月`;
   } else {
     updateSelection();
     clearSelectedClass();
@@ -413,7 +413,7 @@ function change() {
     });
     
     const headerCell = document.querySelector('.header-cell');
-    headerCell.textContent = `${year} 年 ${month} 月  `;    
+    headerCell.textContent = `${year}年  ${month}月`;    
   }
   //below is to highlight the name previously selected in the change function
   highlightSelectedName(temp_name);
