e2621d5757d1e3bcd037897251a09ce145e527cd
kueifeng <kueifeng7166@gmail.com>
2024-09-14 14:46:36 +0800

    add mouse effect

--- script.js
+++ script.js
@@ -964,6 +964,28 @@ document.getElementById('myVideo').addEventListener('ended', function() {
 }, false);
 
 
+document.addEventListener('mousemove', (e) => {
+  const trail = document.createElement('div');
+  trail.classList.add('trail');
+  document.body.appendChild(trail);
+
+  trail.style.left = e.pageX + 'px';
+  trail.style.top = e.pageY + 'px';
+
+  setTimeout(() => {
+
+    trail.remove();
+
+  },500);
+
+});
+
+
+
+
+
+
+
 
 btn.addEventListener('change', change);
 createheadercell(year, month, day);
