74194e69ddf8fb5f48ba9b0f54082c34b73ad76e
kueifeng <kueifeng7166@gmail.com>
2024-10-03 16:12:04 +0800

    add last updated time2

--- update_time.js.js
+++
@@ -1,1 +0,0 @@
-const lastUpdated = '2024-10-03 14:30:00';  // This will be updated by the script
--- update_time.ps1
+++ update_time.ps1
@@ -5,6 +5,6 @@ $current_time = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
 Set-Content -Path "update_time.js" -Value "const lastUpdated = '$current_time';"
 
 # Step 3: Use Git to add, commit, and push the changes
-git add update_time.js
-git commit -m "Updated website time: $current_time"
-git push origin main
+#git add update_time.js
+#git commit -m "Updated website time: $current_time"
+#git push origin main
