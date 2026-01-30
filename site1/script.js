d6abb5ab9f300ad3eaca70d59b51307d8b926c21
unknown <kueifeng7166@gmail.com>
2026-01-30 10:01:22 +0800

    list

--- site1/script.js
+++ site1/script.js
@@ -2260,7 +2260,6 @@ namePicker.addEventListener('scroll', () => {
     'sct@anws.gov.tw':'孫景泰',
     'folra679@gmail.com':'官郁庭',
     'jihyaos@gmail.com':'張日曜',
-    'allen99lo@gmail.com':'羅應順',
     'chihung24@gmail.com':'張啟鴻',
     'dick710209@gmail.com':'陳鈞緯',
   };
@@ -2345,7 +2344,7 @@ namePicker.addEventListener('scroll', () => {
         highlightSelectedName(selectedName);
       });
 
-        if (username !== "詹文欽" && username !== "羅應順" && username !== "張啟鴻" && username !== "張日曜" && username !== "孫景泰" && username !== "張哲維" && username !== "陳鈞緯") {
+        if (username !== "詹文欽"  && username !== "張啟鴻" && username !== "張日曜" && username !== "孫景泰" && username !== "張哲維" && username !== "陳鈞緯") {
 
             const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
 
